f3c7e3b4e9a326cc265b2e4bc1133a73eb661ddb
Fangning Luo <fangningluo@gmail.com>
2025-03-06 20:59:09 -0800

    finished HW4/Q2 R Shiny app

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(lubridate)
 library(bigrquery)  # For BigQuery integration
 library(readr)  # For loading RDS file
 
@@ -48,7 +49,212 @@ con_bq <- dbConnect(
   billing = "biostat-203b-2025-winter"
 )
 
+# Functions to retrieve ADT information and plot ADT
+get_patient_info_for_title <- function(connection, id) {
+  admissions <- tbl(con_bq, "admissions") |>
+    filter(subject_id == id)
+  
+  patient_info_data <- tbl(con_bq, "patients") |>
+    filter(subject_id == id) |>
+    left_join(admissions, by = "subject_id") |>
+    select(subject_id, gender, anchor_age, race) |>
+    distinct() |>
+    collect()
+  
+  plot_title <- paste(
+    "Patient ", pull(patient_info_data, subject_id)[1], 
+    ", ", pull(patient_info_data, gender)[1], 
+    ", ", pull(patient_info_data, anchor_age)[1], 
+    " years old, ", tolower(pull(admissions, race)[1])
+  )
+  
+  return(plot_title)
+}
 
+get_diagnoses_for_subtitle <- function(connection, id) {
+  d_icd_diagnoses <- tbl(con_bq, "d_icd_diagnoses")
+  
+  top_3_diagnoses_data <- tbl(con_bq, "diagnoses_icd") |>
+    filter(subject_id == id) |>
+    count(icd_code, sort = TRUE) |>
+    arrange(desc(n)) |>
+    head(3) |>
+    left_join(d_icd_diagnoses, by = "icd_code") |>
+    select(long_title) |>
+    collect()
+  
+  long_title <- pull(top_3_diagnoses_data, long_title)
+  
+  plot_subtitle <- paste(
+    long_title[1], "\n",
+    long_title[2], "\n",
+    long_title[3],
+    sep = ""
+  )
+  
+  return(plot_subtitle)
+}
+
+get_lab_data <- function(connection, id) {
+  Lab_data <- tbl(con_bq, "labevents") |>
+    select(subject_id, charttime) |>
+    filter(subject_id == id) |>
+    distinct() |>
+    collect()
+  
+  return(Lab_data)
+}
+
+get_ADT_data <- function(connection, id) {
+  ADT_data <- tbl(con_bq, "transfers") |>
+    filter(subject_id == id,
+           !is.na(careunit), careunit != "UNKNOWN",
+           !is.na(intime),
+           !is.na(outtime)) |>
+    mutate(is_icu = str_detect(careunit, 'CU')) |>
+    select(subject_id, careunit, intime, outtime, is_icu) |>
+    collect()
+  
+  return(ADT_data)
+}
+
+get_procedure_data <- function(connection, id) {
+  d_icd_procedures <- tbl(con_bq, "d_icd_procedures")
+  
+  Procedure_data <- tbl(con_bq, "procedures_icd") |>
+    filter(subject_id == id) |>
+    left_join(d_icd_procedures, by = c('icd_code', 'icd_version')) |>
+    select(subject_id, chartdate, long_title) |>
+    collect()
+  
+  return(Procedure_data)
+}
+
+draw_adt <- function(connection, id) {
+  plot_title <- get_patient_info_for_title(connection, id)
+  plot_subtitle <- get_diagnoses_for_subtitle(connection, id)
+  
+  Lab <- get_lab_data(connection, id)
+  ADT <- get_ADT_data(connection, id)
+  Procedure <- get_procedure_data(connection, id)
+  
+  adt_plot <- ggplot() +
+    scale_x_datetime(name = "Calendar Time",
+                     limits = c(min(ADT$intime) - days(1), 
+                                max(ADT$outtime))) +
+    scale_y_discrete(name = NULL,
+                     limits = c("Procedure", "Lab", "ADT")) +
+    
+    # Procedure Events as different shapes
+    geom_point(
+      data = Procedure,
+      aes(x = as.POSIXct(chartdate), y = "Procedure", 
+          shape = sub(",.*", "", long_title)),
+      size = 4
+    ) +
+    
+    # Lab Events as Crosses
+    geom_point(
+      data = Lab,
+      aes(x = charttime, y = "Lab"),
+      shape = 3, size = 3
+    ) +
+    
+    # ADT as line segment
+    geom_segment(
+      data = ADT,
+      aes(x = intime, xend = outtime, y = "ADT", yend = "ADT",
+          color = careunit, linewidth = as.factor(is_icu))
+    ) +
+    
+    # Formatting
+    labs(
+      title = plot_title,
+      subtitle = plot_subtitle,
+      color = "Care Unit",
+      shape = "Procedure"
+    ) +
+    
+    # Legend settings: color and shape
+    guides(
+      # Removes the legend for the line thickness (linewidth)
+      linewidth = "none", 
+      # Title for color legend
+      color = guide_legend(title = "Care Unit", ncol = 3),
+      # Title for shape legend
+      shape = guide_legend(title = "Procedure", ncol = 2)
+    ) +
+    
+    theme_minimal() +
+    theme(
+      legend.position = "bottom",
+      legend.box = "vertical",  # Stack legends vertically
+
+      plot.title = element_text(size = 20, face = "bold"),  
+      plot.subtitle = element_text(size = 16),  
+      axis.title.x = element_text(size = 16),  
+      axis.title.y = element_text(size = 16), 
+      axis.text.x = element_text(size = 14, angle = 45, hjust = 1), 
+      axis.text.y = element_text(size = 14),  
+      legend.title = element_text(size = 14, face = "bold"), 
+      legend.text = element_text(size = 12), 
+    )
+  
+  return(adt_plot)
+}
+
+# Functions to retrieve ICU stay information and plot ICU stay
+get_ICU_data <- function(connection, id) {
+  d_items <- tbl(connection, "d_items") |>
+    select(itemid, label, abbreviation) |>
+    filter(abbreviation %in% c("HR", "NBPd", "NBPs", "RR", "Temperature F")) |>
+    mutate(itemid = as.character(itemid))
+  
+  itemids <- d_items |>
+    pull(itemid) |>
+    as.character()
+  
+  ICU <- tbl(connection, "chartevents") |>
+    mutate(itemid = as.character(itemid)) |> # convert to character
+    filter(subject_id == id,
+           itemid %in% itemids) |>
+    select(subject_id, stay_id, charttime, itemid, valuenum) |>
+    left_join(d_items, by = "itemid") |>
+    collect()
+  
+  return(ICU)
+}
+
+draw_icu <- function(connection, id) {
+  ICU <- get_ICU_data(connection, id)
+  
+  icu_plot <- ggplot(ICU, aes(x = charttime, y = valuenum, color = abbreviation)) +
+    geom_point() +
+    geom_line() +
+    facet_grid(abbreviation ~ stay_id, scales = "free") +
+    
+    labs(
+      title = paste("Patient", ICU$subject_id[1], "ICU stays - Vitals"),
+      x = "",
+      y = ""
+    ) +
+    
+    theme_light() +
+    theme(
+      legend.position = "none",
+      
+      plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+      axis.title.x = element_text(size = 18, face = "bold"),
+      axis.title.y = element_text(size = 18, face = "bold"), 
+      axis.text.x = element_text(size = 14, angle = 35, hjust = 1),
+      axis.text.y = element_text(size = 14), 
+      strip.text = element_text(size = 16, face = "bold"), 
+      strip.text.y = element_text(size = 14, face = "bold")
+    ) +
+    scale_x_datetime(date_labels = "%b %d %H:%M")
+  
+  return(icu_plot)
+}
 
 # Define UI
 ui <- fluidPage(
@@ -125,7 +331,6 @@ ui <- fluidPage(
           ),
           
           mainPanel(
-            # Output placeholders for patient search
             textOutput("patient_info_output"),
             plotOutput("patient_plot")
           )
@@ -294,50 +499,39 @@ server <- function(input, output, session) {
     }
   })
   
-  # Reactive variable to store patient data
-  patient_data <- reactiveVal(NULL)
-  
   # Handle patient lookup when Submit button is clicked
   observeEvent(input$submit_id, {
-    subject_id <- input$subject_id
-    if (is.null(subject_id) || !(subject_id %in% cohort_data$subject_id)) {
+    id <- input$subject_id
+    
+    # check entered subject ID is valid
+    if (is.null(id) || !(id %in% cohort_data$subject_id)) {
       output$patient_info_output <- renderText(
         "Please enter a valid Patient ID.")
       return()
     }
     
-    # Placeholder for fetching data from BigQuery
-    fetched_data <- data.frame(
-      time = seq(1, 10),
-      ADT_value = rnorm(10, mean = 50, sd = 10),
-      ICU_value = rnorm(10, mean = 100, sd = 15)
-    )
-    
-    patient_data(fetched_data)
-    
     output$patient_info_output <- renderText(
-      paste("Patient ID:", subject_id, "data retrieved."))
+      paste("Patient ID:", id, "data retrieved.")
+    )
   })
   
   # Generate plot based on selected patient_info_type
   output$patient_plot <- renderPlot({
-    data <- patient_data()
-    if (is.null(data)) return(NULL)  # If no data, return NULL
+    id <- input$subject_id
+    
+    if (is.null(id) || !(id %in% cohort_data$subject_id)) return()
+    
+    id <- as.integer(id)
     
     if (input$patient_plot_type == "ADT") {
-      ggplot(data, aes(x = time, y = ADT_value)) +
-        geom_line(color = "blue") +
-        geom_point(color = "blue") +
-        labs(title = "ADT Data Over Time", x = "Time", y = "ADT Value") +
-        theme_minimal()
-    } else {
-      ggplot(data, aes(x = time, y = ICU_value)) +
-        geom_line(color = "red") +
-        geom_point(color = "red") +
-        labs(title = "ICU Data Over Time", x = "Time", y = "ICU Value") +
-        theme_minimal()
+      
+      draw_adt(con_bq, id)
+      
+    } else if (input$patient_plot_type == "ICU stays"){
+      
+      draw_icu(con_bq, id)
     }
-  })
+  }, height = 600)
     
 }
 
